42980aea903da89b8a526ed80bcde4b4ca907cc7
akhud78 <akhudiakov@gmail.com>
2024-01-04 20:37:58 +0300

    fix sntp

--- wifi.h
+++ wifi.h
@@ -1,8 +1,6 @@
 #ifndef WIFI_H
 #define WIFI_H
 
-#include "sdkconfig.h"
-
 /* The examples use WiFi configuration that you can set via project configuration menu
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
